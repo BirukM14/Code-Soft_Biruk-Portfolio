d8ee9324bb638f31aad09f13e963fbe51580f841
BirukM14 <gbiruka14@gmail.com>
2025-02-17 19:29:29 +0300

    refactor: simplify server configuration and update port handling

--- server/server.js
+++ server/server.js
@@ -1,10 +1,7 @@
-// server.js
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const config = require('./config/config'); // Import config file
-const route= require('./routes/route')
+const route = require('./routes/route'); // Import routes
 
 const app = express();
 
@@ -15,19 +12,8 @@ app.use(bodyParser.json());
 // Routes
 app.use('/api/contact', route); // Add the user routes
 
-// Connect to MongoDB using the connection string from config
-mongoose.connect(config.dbUri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((err) => {
-    console.log('Failed to connect to MongoDB:', err);
-  });
-
 // Start the server
-app.listen(config.port, () => {
-  console.log(`Server running on port ${config.port}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
